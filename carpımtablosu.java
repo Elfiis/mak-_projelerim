55a778645c90731e497fd4dcda334ac999187a96
Elfiis <113730391+Elfiis@users.noreply.github.com>
2022-11-09 15:52:51 +0300

    Add files via upload

---
+++ carpımtablosu.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author elfis
+ */
+
+public class carpımtablosu {
+
+   
+    public static void main(String[] args) {
+     for(int i=1;i<10;i++){
+         System.out.println(i+"'lerin çarpımı");
+      for (int j =0;j<=10;j++){
+          System.out.println(i+"*"+j+"="+(i*j));
+      }
+     
+     
+     }
+     
+    }
+    
+}
